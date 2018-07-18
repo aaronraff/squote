479028bdda139215c98cddfe5b1b3de82950739e
Aaron Raff <aaronraffdev@gmail.com>
2018-07-17 22:23:19 -0400

    Add Firebase register

--- app/src/main/java/com/aaronraffdev/squote/RegisterActivity.java
+++ app/src/main/java/com/aaronraffdev/squote/RegisterActivity.java
@@ -1,13 +1,68 @@
 package com.aaronraffdev.squote;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        auth = FirebaseAuth.getInstance();
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        final EditText email = findViewById(R.id.email_register);
+        final EditText password = findViewById(R.id.password_register);
+        Button submitButton = findViewById(R.id.submit_register);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameText = email.getText().toString();
+                String passwordText = password.getText().toString();
+                registerUser(usernameText, passwordText);
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            user = auth.getCurrentUser();
+                            Log.d("AUTH", "Successfully registered an account.");
+                        } else {
+                            Toast.makeText(RegisterActivity.this, task.getResult().toString(), Toast.LENGTH_SHORT);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, e.toString(), Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
