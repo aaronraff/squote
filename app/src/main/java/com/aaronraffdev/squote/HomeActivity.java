d36cafb24e42edf3a119cdd40f00be57dcb510f8
Aaron Raff <aaronraffdev@gmail.com>
2018-07-17 21:26:02 -0400

    Add login page

--- app/src/main/java/com/aaronraffdev/squote/HomeActivity.java
+++ app/src/main/java/com/aaronraffdev/squote/HomeActivity.java
@@ -3,24 +3,30 @@ package com.aaronraffdev.squote;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
     private IEXApiClient apiClient;
     private String baseURL = "https://api.iextrading.com/1.0/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiClient = retrofit.create(IEXApiClient.class);
 
         setListeners();
     }
 
-
     private void setListeners() {
-
+        EditText symbolField = findViewById(R.id.symbol);
+        symbolField.addTextChangedListener(new SymbolWatcher(symbolField, apiClient, this));
     }
 }
