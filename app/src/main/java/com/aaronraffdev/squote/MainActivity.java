281c4331ae03042d50938da6c484f1b6bba26f35
Aaron Raff <aaronraffdev@gmail.com>
2018-07-17 21:44:05 -0400

    Added register page

--- app/src/main/java/com/aaronraffdev/squote/MainActivity.java
+++ app/src/main/java/com/aaronraffdev/squote/MainActivity.java
@@ -1,8 +1,12 @@
 package com.aaronraffdev.squote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,12 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         setListeners();
     }
 
 
     private void setListeners() {
-
+        TextView goToRegister = findViewById(R.id.go_to_register);
+        goToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
