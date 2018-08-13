a50118eee3c9c817a49ee9e4a9d2c8ef9366a59b
Aaron Raff <aaronraffdev@gmail.com>
2018-08-13 19:59:00 -0400

    Create properties object

--- squote-service/main.java
+++ squote-service/main.java
@@ -1,2 +1,15 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class main {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+
+        try (FileInputStream in = new FileInputStream("app.config")) {
+            props.load(in);
+        } catch (Exception ex) {
+            System.out.print(ex);
+        }
+
+    }
 }
